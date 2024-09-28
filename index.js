180dee34af946343a62260b57fa5a4992356ec62
krish <krishnathkkar072004@gmail.com>
2024-09-27 20:26:35 -0400

    updated file

--- index.js
+++ index.js
@@ -7,14 +7,12 @@ app.get('/hello', (req, res) => {
     res.send('Hello Express JS');
 });
 
-// Route to handle GET /user with query parameters
 app.get('/user', (req, res) => {
     const firstname = req.query.firstname || 'Krishna';
     const lastname = req.query.lastname || 'Thakkar';
     res.json({ firstname, lastname });
 });
 
-// Route to handle POST /user with path parameters
 app.post('/user/:firstname/:lastname', (req, res) => {
     const { firstname, lastname } = req.params;
     res.json({ firstname, lastname });
